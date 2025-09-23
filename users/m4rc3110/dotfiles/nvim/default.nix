e4bebe4fda6a705009606e6bb1873575ae8a7298
M4rc3110 <git@m4rc3110.foo>
2025-09-23 21:42:30 +0200

    Fix nixd

--- users/m4rc3110/dotfiles/nvim/default.nix
+++ users/m4rc3110/dotfiles/nvim/default.nix
@@ -1,7 +1,18 @@
+{ inputs, hostName, ... }:
 {
   nvim = {
     enable = true;
     packageNames = [ "nvim" ];
+    packageDefinitions.replace = {
+      nvim = { ... }: {
+        extra = {
+          nixdExtras = {
+            nixos_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").nixosConfigurations.${hostName}.options'';
+            home_manager_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").nixosConfigurations.${hostName}.options.home-manager.users.type.getSubOptions []'';
+          };
+        };
+      };
+    };
   };
   home.sessionVariables = {
     EDITOR = "nvim";
